8e0963563cab02b2257ab390c6b2b73607bebb99
sihwankim <sihwan319@gmail.com>
2023-03-10 15:32:46 +0900

    online shop project start build file structure & npm moudule(express, nodemon) install

---
+++ app.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+
+const authRoutes = require('./routes/auth.routes');
+
+const app = express();
+
+app.set('view engine', 'ejs'); // view 템플릿 형식 설정.
+app.set('views', path.join(__dirname, 'views')); // __dirname 경로에 대한 설정, views 경로 폴더에 대한 설정.
+
+app.use(authRoutes);
+
+app.listen(3000);
