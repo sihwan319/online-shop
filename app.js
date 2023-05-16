6a06ef956a261ce4e4d397af253e844b774b0121
sihwankim <sihwan319@gmail.com>
2023-05-16 14:50:11 +0900

    online shop set base.css

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const app = express();
 app.set('view engine', 'ejs'); // view 템플릿 형식 설정.
 app.set('views', path.join(__dirname, 'views')); // __dirname 경로에 대한 설정, views 경로 폴더에 대한 설정.
 
+app.use(express.static('public')); // 정적 폴더 경로 설정.
+
 app.use(authRoutes);
 
 app.listen(3000);
