c3eaf64fc38778526d1574d51032f6ac611a67a9
sihwankim <sihwan319@gmail.com>
2023-05-17 14:14:41 +0900

    signup.ejs data store

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.set('view engine', 'ejs'); // view 템플릿 형식 설정.
 app.set('views', path.join(__dirname, 'views')); // __dirname 경로에 대한 설정, views 경로 폴더에 대한 설정.
 
 app.use(express.static('public')); // 정적 폴더 경로 설정.
+app.use(express.urlencoded({extended: false}));
 
 app.use(authRoutes);
 
